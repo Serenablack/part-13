12167fabac50d05470681efee210c37b7b9a389f
LazyProgrammer21 <tamang.subash36@gmail.com>
2022-07-24 08:12:03 +0545

    feat: use of state passing integer, counter setinterval and array

--- part1/noteApp/components/App.js
+++ part1/noteApp/components/App.js
@@ -1,20 +1,34 @@
 import Myh1 from "./Myh1";
 import React from "react";
+import { useState } from "react";
 
 // using Myh1 component in App
 
 const App = ({ counter }) => {
-  //   return React.createElement("div", {}, [Myh1(), Myh1(), Myh1(), Myh1()]); //passing in array
+  //use state function le euta parameter linxa could be anything object number, string
+  //returns a array of two element; first is the state and other is the function to change the state
+  //real life example, person object use state ma pass garyo vane object create garda each time refresh hunxa with different values of object
+
+  //state ko value change hune bittikai entire component refresh hunxa
+  const [myCount, setMyCount] = useState(1);
+
+  //setTime out function le harek second ma hole component call garxa
+  setTimeout(() => {
+    // let newCount = [1, 2, 3, 4, 5];//renders every second infinite times
+    //setmycount is a method
+    // setMyCount(newCount);
+    setMyCount(myCount + 1);
+  }, 1000);
+
   let name1 = "Subash";
 
   return (
     <div>
-      <h1>{counter}</h1>
-      <Myh1 name="Amir" count={counter} />
+      <h1>{myCount}</h1>
+      <Myh1 name="Amir" count={myCount} />
       <Myh1 name={name1} lastName="Tamang" />
       <Myh1 name="Niru" />
-      <Myh1 />
-      <Myh1 name="Rame ko Nati" lastName="Hamja" />
+      <Myh1 name="Rame" lastName="Hamja" />
     </div>
   );
 };
--- part1/noteApp/index.js
+++ part1/noteApp/index.js
@@ -2,46 +2,13 @@ import App from "./components/App";
 import ReactDOM from "react-dom/client";
 import React from "react";
 
-console.log("Hello from JS!");
-
-//html ko root id vako div ma react id app rakhnu ko lagi:
-//method1
-// ReactDOM.render(React.createElement("div",{},"This is from REACT"),document.querySelector("#root"))
-
-//using function to create react app
-
-// const App=()=>{
-
-//     return React.createElement("div",{},"This is from REACT component")
-
-// }
-
-//react 18 ma yo line le kam gardaina
-// ReactDOM.render(App(), document.querySelector("#root"));
-let myCount = 1;
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <App counter={myCount} />
-  );
-};
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-refresh();
-
-//App render vaneko refresh hunu ho browser ma however in console refresh function duichoti call garda duichoti name
-// print garxa
-//same component lai duichoti render garnu vaneko append hudai jane haina but replace the old one.
-//
-
-//myCount += 1;
-
 // refresh();
 
-// //calling a same function twice to render.
-
 //using setInterval
-setInterval(() => {
-  refresh();
-  myCount++;
-}, 1000);
+// setInterval(() => {
+//   refresh();
+//   myCount++;
+// }, 1000);
