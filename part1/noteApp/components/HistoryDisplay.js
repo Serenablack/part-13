65dc818219e0b022be22c3291383b0c297aafdf1
LazyProgrammer21 <tamang.subash36@gmail.com>
2022-07-24 09:14:41 +0545

    feat: conditional rendering

--- part1/noteApp/components/HistoryDisplay.js
+++ part1/noteApp/components/HistoryDisplay.js
@@ -1,4 +1,6 @@
 const HistoryDisplay = ({ history }) => {
-  return <h1>The history of click is : {history}</h1>;
+  if (history.length > 0) {
+    return <h1>The history of click is : {history}</h1>;
+  } else return <h1>There is no History yet</h1>;
 };
 export default HistoryDisplay;
