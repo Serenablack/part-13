d9a346192a3cacc68194793532ea225d5ca158bc
Lazyprogrammer21 <tamang.subash36@gmail.com>
2022-08-01 06:07:08 -0400

    feat: axios in phonebook 2.11 completed

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,53 +1,45 @@
-import { Filter } from './components/Filter';
-import { PersonForm } from './components/PersonForm';
-import { useState } from "react";
-import Person from './components/Person';
+import { Filter } from "./components/Filter";
+import { PersonForm } from "./components/PersonForm";
+import { useEffect, useState } from "react";
+import Person from "./components/Person";
 
-
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 9865457708 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [filter,setFilter]=useState("");
+  const [filter, setFilter] = useState("");
+  //
+  useEffect(() => {
+    
+    axios.get("http://localhost:3001/persons").then((result) => {
+      setPersons(result.data);
+    });
+  
+  }, [])
+  
 
-
-
+ 
   const handleOnChangeName = (event) => {
     setNewName(event.target.value);
   };
   const handleOnChangeNumber = (event) => {
     const number = event.target.value;
-    // if (String(number) > 10) {
-    //   alert("PhoneNumber cannot be more than 10digits");
-    // } else if (String(number) === "") {
-    //   alert("PhoneNumber cannot be Empty!!");
-    // } else {
-    //   setNewName(number);
-    // }
-    setPhoneNumber(number)
+
+    setPhoneNumber(number);
   };
 
+  const handleOnChangeFilter = (event) => {
+    // console.log(event.target.value);
+    const filtname = event.target.value;
+    setFilter(filtname);
+  };
 
-const handleOnChangeFilter=(event)=>{
-  // console.log(event.target.value);
-  const filtname = event.target.value;
-  setFilter(filtname);
+  let filteredname = persons.filter((person) => person.name.includes(filter));
+  // console.log(filteredname);
 
-}
-
-let filteredname = persons.filter((person)=>person.name.includes(filter));
-// console.log(filteredname);
-
-
-
-
-
-
-
- const addPerson = (event) => {
+  const addPerson = (event) => {
     event.preventDefault();
     // console.log(event.target)
 
@@ -55,30 +47,32 @@ let filteredname = persons.filter((person)=>person.name.includes(filter));
     if (persons.some((Pname) => Pname.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     }
+    else{
+      setPersons([...persons, { name: newName, number: phoneNumber }]);
+      setNewName("");
+      setPhoneNumber("");
+    }
+
    
-    setPersons([...persons, {name: newName,number: phoneNumber,}]);
-    setNewName("");
-    setPhoneNumber("");
-    
   };
-
-
 
   return (
     <div>
       <h2>Phonebook</h2>
-     <Filter   filter={filter} handleOnChangeFilter={handleOnChangeFilter}  />
-     <h2>Add New Contact</h2>
-     <PersonForm   addPerson={addPerson} newName={newName} handleOnChangeName={handleOnChangeName} phoneNumber={phoneNumber} handleOnChangeNumber={handleOnChangeNumber}  />
+      <Filter filter={filter} handleOnChangeFilter={handleOnChangeFilter} />
+      <h2>Add New Contact</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleOnChangeName={handleOnChangeName}
+        phoneNumber={phoneNumber}
+        handleOnChangeNumber={handleOnChangeNumber}
+      />
 
       <h2>Phone Numbers</h2>
 
-    <Person filteredname={filteredname}></Person>
+      <Person filteredname={filteredname}></Person>
     </div>
   );
-
-
 };
 export default App;
-
-  
