4db0cb99f78839ba88769b3c591aea8c74aefe84
Lazyprogrammer21 <tamang.subash36@gmail.com>
2022-09-06 13:19:53 +0545

    feat: project managed

--- part1/noteApp/components/Display.js
+++
@@ -1,5 +0,0 @@
-const Display = (props) => {
-  return <h3>count in Display Component is: {props.showValue} </h3>;
-};
-
-export default Display;
--- part1/noteApp/components/Mybutton.js
+++
@@ -1,5 +0,0 @@
-const Mybutton = ({ buttonHandler }) => {
-  return <button onClick={buttonHandler}> Click button Component</button>;
-};
-
-export default Mybutton;
